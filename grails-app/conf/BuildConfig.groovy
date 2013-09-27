e220e3a1060abd8cd6095569dc9a851d36a92272
Donal Murtagh <domurtag@yahoo.co.uk>
2013-09-27 17:44:18 +0100

    upgraded release plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -32,7 +32,7 @@ grails.project.dependency.resolution = {
     plugins {
         build ":tomcat:$grailsVersion"
 
-        build(':release:2.0.3', ':rest-client-builder:1.0.2') {
+        build(':release:2.2.1', ':rest-client-builder:1.0.2') {
             export = false
         }
     }
