bc32e95d48c2ef842ceb0983d73e1b5f18156cf6
Donal Murtagh <domurtag@yahoo.co.uk>
2013-09-27 17:00:58 +0100

    upgraded to Grails 2.1.5 and moved to GitHub

---
+++ grails-app/controllers/grails/plugin/simplecaptcha/SimpleCaptchaController.groovy
@@ -0,0 +1,133 @@
+package grails.plugin.simplecaptcha
+
+import java.awt.Color
+import java.awt.Font
+import java.awt.Graphics2D
+import java.awt.RenderingHints
+import java.awt.geom.Rectangle2D
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+import javax.servlet.http.Cookie
+import org.apache.commons.lang.RandomStringUtils
+
+class SimpleCaptchaController {
+
+    def simpleCaptchaService
+    def grailsApplication
+
+    private static final DEFAULT_CAPTCHA_CHARS = ('A'..'Z').step(1).join()
+    private static final DEFAULT_FONT = 'Serif'
+
+    private static final ONE_DAY_IN_SECONDS = 60 * 60 * 24
+    /**
+     * If there's already a CAPTCHA in the session, re-use it, otherwise generate a new one. This allows us to show
+     * the same CAPTCHA in several places on the same page (which is not possible with ReCaptcha).
+     */
+    def captcha = {
+        def captcha = session[SimpleCaptchaService.CAPTCHA_IMAGE_ATTR] ?: newCaptcha()
+        ImageIO.write(captcha, "PNG", response.outputStream)
+    }
+
+    private static Graphics2D createGraphic(BufferedImage image, Font font) {
+        Graphics2D g2d = image.createGraphics()
+        g2d.font = font
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+        g2d
+    }
+
+    /**
+     * Creates a new CAPTCHA, and stores the CAPTCHA image and its solution in the session
+     * @return The CAPTCHA image
+     */
+    private newCaptcha() {
+        response.contentType = "image/png"
+        response.setHeader("Cache-control", "no-cache")
+
+        String captchaCharset = grailsApplication.config?.simpleCaptcha?.chars ?: DEFAULT_CAPTCHA_CHARS
+        String fontName = grailsApplication.config?.simpleCaptcha?.font ?: DEFAULT_FONT
+        
+        final int height = getParamValue(200, 'height')
+        final int width = getParamValue(200, 'width')
+        final int fontSize = getParamValue(24, 'fontSize')
+        final int captchaLength = getParamValue(6, 'length')
+        final int bottomPadding = getParamValue(16, 'bottomPadding')
+        final int lineSpacing = getParamValue(10, 'lineSpacing')
+
+        // Generate the CAPTCHA solution
+        String captchaSolution = RandomStringUtils.random(captchaLength, captchaCharset.toCharArray())
+
+        System.setProperty("java.awt.headless", "true")
+        BufferedImage captchaImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+        Font font = new Font(fontName, Font.BOLD, fontSize)
+
+        Graphics2D g2d = createGraphic(captchaImage, font)
+        Rectangle2D fontRect = font.getStringBounds(captchaSolution, g2d.fontRenderContext)
+
+        // Create a graphic "space" pixels wider and taller than the the font
+        captchaImage = new BufferedImage((int) fontRect.width + bottomPadding, (int) fontRect.height + bottomPadding,
+                BufferedImage.TYPE_INT_RGB)
+        g2d = createGraphic(captchaImage, font)
+
+        // Draw the background
+        g2d.color = Color.WHITE
+        g2d.fillRect(0, 0, width, height)
+
+        // Draw the lines
+        g2d.color = Color.GRAY
+
+        int y1 = lineSpacing
+        int x2 = lineSpacing
+
+        int x1 = 0
+        int y2 = 0
+
+        while (x1 < width || x2 < width || y1 < height || y2 < height) {
+            g2d.drawLine(x1, y1, x2, y2)
+            if (y1 < height) {
+                x1 = 0
+                y1 += lineSpacing
+            } else if (x1 < width) {
+                y1 = height
+                x1 += lineSpacing
+            } else {
+                x1 = width
+                y1 = height
+            }
+
+            if (x2 < width) {
+                y2 = 0
+                x2 += lineSpacing
+            } else if (y2 < height) {
+                x2 = width
+                y2 += lineSpacing
+            } else {
+                y2 = height
+                x2 = width
+            }
+        }
+
+        // Draw the String
+        g2d.color = Color.BLACK
+        g2d.drawString(captchaSolution, (int) (bottomPadding / 2), (int) (bottomPadding / 4) + (int) fontRect.height)
+
+        if (simpleCaptchaService.storeInSession()) {
+            //	Use the session to store the solution as well as the image itself
+            def session = request.getSession(true)
+            session.setAttribute(SimpleCaptchaService.CAPTCHA_SOLUTION_ATTR, captchaSolution)
+            session.setAttribute(SimpleCaptchaService.CAPTCHA_IMAGE_ATTR, captchaImage)
+        } else {
+            //	Store the hashed solution in a cookie... this does not currently store the image
+            def cookieValue = simpleCaptchaService.encode(captchaSolution).encodeAsURL()
+            Cookie cookie = new Cookie(SimpleCaptchaService.CAPTCHA_SOLUTION_ATTR, cookieValue)
+            cookie.maxAge = ONE_DAY_IN_SECONDS
+			cookie.path = '/'
+			response.addCookie(cookie)
+        }
+        captchaImage
+    }
+
+    private getParamValue(Integer defaultValue, String configKey) {
+        def configFileValue = grailsApplication.config.simpleCaptcha?."$configKey"
+        configFileValue ? configFileValue.toInteger() : defaultValue
+    }
+}
---
+++ grails-app/services/grails/plugin/simplecaptcha/SimpleCaptchaService.groovy
@@ -0,0 +1,88 @@
+package grails.plugin.simplecaptcha
+
+import org.codehaus.groovy.grails.web.servlet.mvc.GrailsWebRequest
+import org.codehaus.groovy.grails.web.util.WebUtils
+
+import java.security.MessageDigest
+import javax.servlet.http.Cookie
+
+class SimpleCaptchaService {
+
+    static final CAPTCHA_SOLUTION_ATTR = 'captcha'
+    static final CAPTCHA_IMAGE_ATTR = 'captchaImage'
+
+    def grailsApplication
+
+    /**
+     * Indicates if the CAPTCHA was solved correctly
+     * @param captchaSolution The CAPTCHA solution provided by the user
+     * @return Indicates whether the CAPTCHA challenge was passed
+     */
+    boolean validateCaptcha(String captchaSolution) {
+
+        GrailsWebRequest webRequest = WebUtils.retrieveGrailsWebRequest()
+
+        if (storeInSession()) {
+            //	extract the value from the session
+            def session = webRequest.session
+            String solution = session[CAPTCHA_SOLUTION_ATTR]
+
+            // remove the CAPTCHA so a new one will be generated next time one is requested
+            session.removeAttribute(CAPTCHA_SOLUTION_ATTR)
+            session.removeAttribute(CAPTCHA_IMAGE_ATTR)
+
+            captchaSolution ? solution?.compareToIgnoreCase(captchaSolution) == 0 : false
+
+        } else {
+            //	We'll use the cookie instead
+            def request = webRequest.currentRequest
+            String solution = readCookie(CAPTCHA_SOLUTION_ATTR, request)
+
+            def response = webRequest.currentResponse
+
+            //	Remove the cookie if we have a response
+            if (response) {
+                Cookie c = new Cookie(CAPTCHA_SOLUTION_ATTR, null)
+                c.maxAge = 0
+                response.addCookie(c)
+            }
+
+            captchaSolution ? solution == encode(captchaSolution) : false
+        }
+    }
+
+    /**
+     * @return Indicates if session storage is being used
+     */
+    boolean storeInSession() {
+        def pluginConfig = grailsApplication.config.simpleCaptcha
+
+        // Not sure why an explicit null check is necessary, it was submitted as a bug-fix by a user
+        if (pluginConfig.storeInSession != null) {
+            return pluginConfig.storeInSession.asBoolean()
+        }
+        true
+    }
+
+    /**
+     * Creates an MD5 digest version of a lower-case version of a string
+     * @param stringToEncode
+     * @param The encoded string
+     */
+    String encode(String stringToEncode) {
+        def digest = MessageDigest.getInstance("MD5")
+        new BigInteger(1, digest.digest(stringToEncode.toLowerCase().bytes)).toString(16).padLeft(32, "0")
+    }
+
+    /**
+     * Reads a cookie
+     * @param name The name used to store the cookie
+     * @return The value of the cookie, or null if no such value exists
+     */
+    private String readCookie(String name, request) {
+
+        //	Get all the cookies
+        def cookie = request.cookies?.find {it.name == name}
+        cookie ? cookie.value?.decodeURL() : null
+    }
+}
